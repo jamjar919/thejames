26282885e4ab25a3dae0732912c893b181e5bea3
James Paterson <james@mjsquare.com>
2020-01-28 21:44:15 +0000

    starting working build

---
+++ src/components/App.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+const App = () => (
+    <div>The React Env is now set. Served from react component.</div>
+);
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+     module: {
+        rules: [
+            {
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({template: './src/index.html'})
+    ],
+    devServer:{
+        port:9000
+    }
+};
