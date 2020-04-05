beb515b9c17bf64a755abfb41532df63df82c815
James Paterson <james@mjsquare.com>
2020-04-05 19:38:48 +0100

    add additional files

---
+++ src/components/effects/Crt.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import ClassNames from 'classnames';
+
+import './css/crt.scss';
+
+const Crt = props => {
+    const [off, setOff] = useState(false);
+
+    return (
+        <div className={ClassNames("crt", off ? "off" : "")} onClick={() => setOff(!off)}>
+            {props.children}
+        </div>
+    );
+};
+
+Crt.propTypes = {
+    children: PropTypes.node.isRequired
+}
+
+export default Crt;
