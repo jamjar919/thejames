af89bdf5c7cbb6a84aae16ed742867951e37e630
James Paterson <james@mjsquare.com>
2020-04-05 19:38:29 +0100

    better glitch effect

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import Glitch from './Glitch';
+import Glitch from './effects/Glitch';
+import Crt from './effects/Crt'
 
 import './normalize.scss';
 import './app.scss';
 
 const App = () => (
-    <div className="container">
-        <Glitch>James Paterson</Glitch>
-    </div>
+    <Crt>
+        <div className="fullpage">
+            <div className="centeredContainer">
+                <Glitch>://james paterson</Glitch>
+            </div>
+        </div>
+    </Crt>
 );
 
 export default App;
