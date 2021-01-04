831083d5435aac99adea72c24524b07794cc08f8
James Paterson <jpaterson@tripadvisor.com>
2021-01-04 17:01:03 +0000

    upadate likedin, prerendering + meta desc

---
+++ webpack.prod.config.js
@@ -0,0 +1,42 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            },
+            {
+                test: /\.scss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    { loader: 'css-loader', options: { url: false, sourceMap: true } },
+                    { loader: 'sass-loader', options: { sourceMap: true } }
+                ]
+            },
+            {
+                test: /\.svg$/i,
+                use: ['@svgr/webpack']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: '!!prerender-loader?string!./src/index.html'
+        }),
+        new MiniCssExtractPlugin({ filename: "style.css" })
+    ],
+    devServer:{
+        port:9000
+    },
+    mode : 'production'
+};
